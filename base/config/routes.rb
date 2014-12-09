7dc7bbe13db107c80573c9803697089fca75550a
Chris <chris@codecreationsinc.com>
2014-12-09 07:53:38 -0600

    rails_admin now mounts under admins instead of admin

--- base/config/routes.rb
+++ base/config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   root 'pages#home'
 
-  #devise_for :users
   devise_for :users, controllers: { sessions: "users/sessions" }
   devise_for :admins
 
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  mount RailsAdmin::Engine => '/admins', as: 'rails_admin'
 
   devise_scope :user do 
     get 'dashboard' => 'users/sessions#dashboard'
