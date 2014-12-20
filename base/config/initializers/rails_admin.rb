334c01696e2f5a0400dc8b152602f3b11603e432
Chris <chris-roerig@users.noreply.github.com>
2014-12-20 10:53:00 -0600

    + Rails 4 Enum support to rails_admin initializer

--- base/config/initializers/rails_admin.rb
+++ base/config/initializers/rails_admin.rb
@@ -37,3 +37,26 @@ RailsAdmin.config do |config|
     # history_show
   end
 end
+
+#Rails 4 Enum support
+module ActiveRecord
+  module RailsAdminEnum
+    def enum(definitions)
+      super
+
+      definitions.each do |name, values|
+        define_method("#{ name }_enum") { self.class.send(name.to_s.pluralize).to_a }
+
+        define_method("#{ name }=") do |value|
+          if value.kind_of?(String) and value.to_i.to_s == value
+            super value.to_i
+          else
+            super value
+          end
+        end
+      end
+    end
+  end
+end
+
+ActiveRecord::Base.send(:extend, ActiveRecord::RailsAdminEnum)
